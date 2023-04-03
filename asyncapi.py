bb96e9ea2c1b611bb7706ddc5ab0d23d74a4dd1a
Sagar Hallikar <118191069+sagarhallikar@users.noreply.github.com>
2023-04-03 12:43:37 +0530

    Base code for Async API using Flask

---
+++ asyncapi.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, jsonify,redirect
+import asyncio
+
+app = Flask(__name__)
+app.route('/')
+def index():
+    return redirect('/asyncapi')
+
+async def async_task():
+    await asyncio.sleep(1)
+    return {'status': 'success'}
+
+@app.route('/asyncapi', methods=['GET'])
+async def async_view():
+    result = await async_task()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
